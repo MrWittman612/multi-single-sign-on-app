cc7ec8b3f29cde4ddd7d33720ed913be031075d7
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-10-20 01:43:17 -0500

    add if statement to handle post herku push and the build/client flie

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/connectDB');
 
 const app = express();
 
 connectDB();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (_req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 8300;
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server Started on port ${port}`);
 });
